eca48dff116b65f771f223eeea5759837b406274
unknown <misefe1964@gmail.com>
2021-03-24 22:13:23 -0300

    Falta rotinas de delay e finalização

--- main.asm
+++ main.asm
@@ -2,9 +2,16 @@
 ; Organizacao e Arquitetura de Computadores
 ; Nomes: Jhennifer Matias e Milena Fernandes
 ; Professor: Roderval Marcelino
-; Descrição: Timer de no maximo 9s 
- 
-    .org 0010h
+; Descrição: 4 Timers de no maximo 9s
+.define
+    TIMER1 00H
+    TIMER2 01H
+    TIMER3 02H
+    TIMER4 03H
+
+.org 0010h
+
+    MVI D, 00H      ; registrador D armazena número do timer atual
 
     LXI H, 1000H
     MVI M, 77H      ; 77h resulta no numero 0 no display
@@ -36,39 +43,59 @@
     INR L
     MVI M, 6FH      ; 6FH resulta no numero 9 no display
 
+    MVI A, 11H              ; mostra numero do timer
+    OUT 07H
+    IN TIMER1               ; le numero para a primeira contagem
+    CALL COMECA_CONTAGEM    ; inicia o timer
+    MVI A, 3EH              ; mostra numero do timer
+    OUT 07H
+    IN TIMER2               ; le numero para a segunda contagem
+    CALL COMECA_CONTAGEM    ; inicia o timer
+    MVI A, 6EH              ; mostra numero do timer
+    OUT 07H
+    IN TIMER3               ; le numero para a terceira contagem
+    CALL COMECA_CONTAGEM    ; inicia o timer
+    MVI A, 4DH              ; mostra numero do timer
+    OUT 07H
+    IN TIMER4               ; le numero para a quarta contagem
+    CALL COMECA_CONTAGEM    ; inicia o timer
+    JMP FIM                 ; fim do programa
 ; porta de entrada associada ao teclado eh 03H
 ; lê numero total de segundos:
 
-    IN 03H
+COMECA_CONTAGEM:
+    MOV B, A                   ; registrador B armazena quantos segundos faltam
 
-    SBI 30H         ; transforma numero inserido em binario puro
-    MOV B, A        ; registrador B armazena quantos segundos faltam
+    MOV L, A                   ; M contem o endereco do numero de segundos inicial
 
-    MOV L, A        ; M contem o endereco do numero de segundos inicial
-
-JMP LOOP
+    CALL LOOP
+    RET
 
 
-DELAY_UMS: OUT 10H
+DELAY_UMS: 
 ; FAZER ROTINA DE DELAY AQUI
     RET                        ; salta para continuacao do loop
 
-LOOP: MOV A, M                 ; passa para o acumulador o numero para o display
+LOOP: 
+    MOV A, M                   ; passa para o acumulador o numero para o display
     OUT 00H                    ; mostra o numero de segundos faltantes
     CALL DELAY_UMS
     MOV A, B                   ; acumulador recebe numero de segundos faltantes
     DCR A                      ; decrementa o numero de segundos faltantes
     DCR L                      ; decrementa endereco de memoria para ir para o proximo numero
-    JNZ LOOP
-    JMP FIM
+    MOV A, M                   ; passa para o acumulador o numero para o display
+    OUT 00H                    ; mostra o numero de segundos faltantes
+    JNZ LOOP                   ; terminou de decrementar? 
+    RET                        ; se sim, le o proximo numero
 
 
-FIM: MOV A, M
-    OUT 00H
+FIM: 
+    MOV A, M
+    OUT 00H                  
 ; FAZER ROTINA DE FINALIZACAO AQUI
 
 HLT
 ; PARA EXECUTAR: 
 ; DISPLAY 7-SEG EH PORTA 00H
-; FILEIRA DO PAINEL DE LEDS: PORTA 10H (PODE SER USADO NA ROTINA DE FINALIZACAO)
-; TECLADO: ENTRADA 03H
+; FILEIRA DO PAINEL DE LEDS: PORTA 10H A 13H
+; INTERRUPTORES: ENTRADAS 00H A 03H
